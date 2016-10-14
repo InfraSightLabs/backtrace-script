e9dd60d69b5cd7795cba2e1de83a3b77188d712a
WOOLAN <WOOLAN@piratized.se>
2016-10-14 09:36:13 +0200

    0.1.6

--- backtrace.js
+++ backtrace.js
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2016 Fredrik Ullner <fredrik.ullner@infrasightlabs.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 (function(global, Backtrace) {
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
         module.exports = new Backtrace(global, undefined);
@@ -15,8 +39,8 @@
         global.backtrace = new Backtrace(global, url);
     }
 })(window, function(global, captureUrl) {
-    var VERSION = '0.1.5';
-    var INTERVAL = 10000;
+    var VERSION = '0.1.6';
+    var INTERVAL = 30000;
     var metadata = {};
     var queue = [];
     var timer;
